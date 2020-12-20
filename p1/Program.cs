5d58d9555bcf3814e670c32cc85a06427d68223a
yousef9661999 <76272512+yousef9661999@users.noreply.github.com>
2020-12-20 19:47:39 +0300

    yousef m1-94

---
+++ p1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "lux";
+            string superPower1 = "Ball fire";
+            string superPower2 = "Blizzard";
+            int age = 66;
+            double characterspeed = 45.5;
+            Console.WriteLine("me name is {0} me spells is {1} and {2} i have {3} old .me speed os {4}", characterName, superPower1, superPower2, age, characterspeed);
+        }
+    }
+}
